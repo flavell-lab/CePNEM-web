388ba13ee844c6d5ac7ba3d712fe9cdc2c44e89d
Adam Atanas <adamatanas@gmail.com>
2022-11-18 23:47:51 -0500

    Initialized index page

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>CePNEM Data Browser</title>
+</head>
+<body>
+
+
+<div id="main" style="background-color:rgba(20,20,20,0.8)">
+<h1>CePNEM Data Browser</h1>
+<h3>Brain-wide representations of behavior spanning multiple timescales
+    and states in <i>C. elegans</i>
+</h3>

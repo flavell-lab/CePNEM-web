32cf23e5dba8fd33b063aa03e8a1681d53ac3dc6
Adam Atanas <adamatanas@gmail.com>
2022-11-28 12:54:17 -0500

    Initialized table

--- src/index.php
+++ src/index.php
@@ -11,3 +11,28 @@
 <h3>Brain-wide representations of behavior spanning multiple timescales
     and states in <i>C. elegans</i>
 </h3>
+
+<?php
+    $data = file_get_contents('data/summary.json');
+
+    $fields = ['is_stim', 'num_neurons', 'num_labeled', 'max_t', 'num_encoding_changes']
+
+    $decoded_data = json_decode($data, true);
+
+    $count = 0;
+    foreach($decoded_data as $dataset => $dataset_data) {
+        if ($count % 2 == 0) {
+            echo "<tr>";
+        } else {
+            echo "<tr class='alt'>";
+        }
+
+        echo "<td> $dataset </td>";
+        for ($i = 0; $i < count($fields); $i++) {
+            echo "<td> $dataset_data[$fields[$i]] </td>"
+        }
+        $count ++;
+        echo "</tr>";
+    }
+?>
+

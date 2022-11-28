edcaba961371606e155f959dbf641c7909e7a6f7
EC2 Default User <ec2-user@ip-172-31-86-84.ec2.internal>
2022-11-28 18:25:49 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -12,14 +12,27 @@
     and states in <i>C. elegans</i>
 </h3>
 
+<center>
+<p><strong>Datasets</strong></p>
+<table>
+
 <?php
     $data = file_get_contents('data/summary.json');
 
-    $fields = ['is_stim', 'num_neurons', 'num_labeled', 'max_t', 'num_encoding_changes']
+    $fields = ['dataset_type', 'num_neurons', 'num_labeled', 'max_t', 'num_encoding_changes'];
 
     $decoded_data = json_decode($data, true);
 
     $count = 0;
+
+    echo "<tr>";
+    echo "<th> Dataset </th>";
+    echo "<th> Dataset Type </th>";
+    echo "<th> # Neurons </th>";
+    echo "<th> # Labeled Neurons </th>";
+    echo "<th> Max timepoint </th>";
+    echo "<th> # Encoding Changes </th>";
+
     foreach($decoded_data as $dataset => $dataset_data) {
         if ($count % 2 == 0) {
             echo "<tr>";
@@ -28,11 +41,14 @@
         }
 
         echo "<td> $dataset </td>";
-        for ($i = 0; $i < count($fields); $i++) {
-            echo "<td> $dataset_data[$fields[$i]] </td>"
+	for ($i = 0; $i < count($fields); $i++) {
+	    $display_data = $dataset_data[$fields[$i]];
+            echo "<td> $display_data </td>";
         }
         $count ++;
         echo "</tr>";
     }
 ?>
+</table>
+</center>
 

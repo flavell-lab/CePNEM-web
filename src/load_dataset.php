18a260d6d763f7ce1f7a6adc6e4bba9bb091a69d
Adam Atanas <adamatanas@gmail.com>
2022-12-06 12:45:08 -0500

    Add encoding table to dataset viewing page.

--- src/load_dataset.php
+++ src/load_dataset.php
@@ -10,6 +10,7 @@
     $decoded_data = json_decode($data, true);
     $trace_array = $decoded_data["trace_array"];
     $timestep = $decoded_data["avg_timestep"];
+    $categorization = $decoded_data["neuron_categorization"];
 
     $multiplier = function($value) use ($timestep) {
         return $value * $timestep;
@@ -22,6 +23,11 @@
     $body_curvature = $decoded_data["body_curvature"];
 
     $type = $decoded_data['dataset_type'];
+
+    $subcats = array();
+    $subcats["v"] = ["fwd", "rev", "fwd_slope_pos", "fwd_slope_neg", "rev_slope_pos", "rev_slope_neg", "rect_pos", "rect_neg"];
+    $subcats["θh"] = ["dorsal", "ventral", "fwd_dorsal", "fwd_ventral", "rev_dorsal", "rev_ventral", "rect_dorsal", "rect_ventral"];
+    $subcats["P"] = ["act", "inh", "fwd_act", "fwd_inh", "rev_act", "rev_inh", "rect_act", "rect_inh"];
 ?>
 
 <html>
@@ -57,11 +63,81 @@
 
     <table>
         <tr>
+            <th scope="col" rowspan=2>Neuron</th>
             <th scope="col" colspan=10>Velocity tuning</th>
             <th scope="col" colspan=10>Head curvature tuning</th>
             <th scope="col" colspan=10>Feeding tuning</th>
             <th scope="col">EWMA</th>
         </tr>
+        <tr>
+            <th>Encoding strength</th>
+            <th>Forwardness</th>
+            <th><b>F</b>wd</th>
+            <th><b>R</b>ev</th>
+            <th><b>F</b>wd slope +</th>
+            <th><b>F</b>wd slope -</th>
+            <th><b>R</b>ev slope +</th>
+            <th><b>R</b>ev slope -</th>
+            <th><b>F</b>wd slope > R</b>ev slope</th>
+            <th><b>F</b>wd slope < R</b>ev slope</th>
+
+            <th>Encoding strength</th>
+            <th>Dorsalness</th>
+            <th><b>D</b>orsal</th>
+            <th><b>V</b>entral</th>
+            <th><b>D</b>orsal during <b>F</b></th>
+            <th><b>V</b>entral during <b>F</b></th>
+            <th><b>D</b>orsal during <b>R</b></th>
+            <th><b>V</b>entral during <b>R</b></th>
+            <th>More <b>D</b> during <b>F</b></th>
+            <th>More <b>V</b> during <b>F</b></th>
+
+            <th>Encoding strength</th>
+            <th>Feedingness</th>
+            <th><b>A</b>ctivated</th>
+            <th><b>I</b>nhibited</th>
+            <th><b>A</b>ct during <b>F</b></th>
+            <th><b>I</b>nh during <b>F</b></th>
+            <th><b>A</b>ct during <b>R</b></th>
+            <th><b>I</b>nh during <b>R</b></th>
+            <th>More <b>A</b> during <b>F</b></th>
+            <th>More <b>I</b> during <b>F</b></th>
+        </tr>
+        <?php
+            $neurons = range(0,count($trace_array))
+
+            foreach ($neurons as $neuron) {
+                echo "<tr>";
+                echo "<td>$neuron</td>";
+
+                foreach (["v", "θh", "P"] as $beh) {
+                    echo "<td>0.0</td>";
+                    echo "<td>0.0</td>";
+                    foreach ($subcats[$beh] as $subcat) {
+                        $table_string = "";
+                        foreach ([1,2] as $rng) {
+                            if (in_array($neuron, $categorization[$rng]["v"]["fwd"])) {
+                                $table_string = $table_string . $rng;
+                            }
+                        }
+                        if ($table_string == "") {
+                            $table_string = "-";
+                            $style = "style='background-color:#400";
+                        } elseif ($table_string == "12") {
+                            $table_string = "1, 2";
+                            $style = "style='background-color:#040";
+                        } else {
+                            $style = "style='background-color:#330";
+                        }
+                        echo "<td style=$style>$table_string</td>";
+                    }
+                }
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        ?>
+
     </table>
 
     <form>

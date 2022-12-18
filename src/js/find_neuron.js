c84fd7a294f92dc0256836ff5ea39020c3e9c6d6
urlicht <jungsu93@gmail.com>
2022-12-18 08:47:24 -0500

    modify tooltip

--- src/js/find_neuron.js
+++ src/js/find_neuron.js
@@ -120,3 +120,5 @@ function clearSelect() {
     }
 }
   
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))

b56fcc78fc503bf31e109b5d2e67556c8b60f932
Alex Hiser <alex.hiser@gmail.com>
2023-07-28 13:24:04 -0400

    Update test.js

--- src/js/test.js
+++ src/js/test.js
@@ -123,7 +123,7 @@ function clearSelect() {
 function downloadSelected(){
     var idsToDownload = $("#dataset_table").bootstrapTable("getSelections")
 
-    alert(JSON.stringify(idsToDownload))
+    alert(JSON.stringify(idsToDownload).id)
 
 }
 

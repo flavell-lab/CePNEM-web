55cbcbdbbd9462e0304fbce08e45457aa2ab3b49
Alex Hiser <alex.hiser@gmail.com>
2023-08-08 12:46:37 -0400

    Update plot.js

--- src/js/plot.js
+++ src/js/plot.js
@@ -7,7 +7,7 @@ function initPlot(plot_element) {
 		yaxis: { title: "Neuron GCaMP (z-scored)", color: "#000000" , autorange: true},
 		yaxis2: { title: "Behavior (see legend for units)", color: "#000000" },
 		showlegend: true,
-		width: 1050,
+		width: 1150,
 		height: 800,	  
 		plot_bgcolor: "#FFF",
 		paper_bgcolor: "#FFF",

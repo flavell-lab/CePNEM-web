b34fe3fdc1934f0b24ffa1be21321fedc555b2a7
Alex Hiser <alex.hiser@gmail.com>
2023-08-09 10:21:33 -0400

    Update plot_dataset.js

--- src/js/plot_dataset.js
+++ src/js/plot_dataset.js
@@ -549,7 +549,7 @@ function find_matches(neuropal_label){
                         
                         if(curr_dataset_uid === dataset_uid){
                             alert(dataset_uid)
-                            $('#small_dataset_table').bootstrapTable('check', i);
+                            $('#small_dataset_table').bootstrapTable('checkBy', {field: 'id', values: [curr_dataset_uid]} );
                         }
                     } else {
                         $('#small_dataset_table').bootstrapTable("hideRow", {uniqueId: curr_dataset_uid});

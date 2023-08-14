2012c3bc185ff09ecade2126a875360b9aef5c18
Alex Hiser <alex.hiser@gmail.com>
2023-08-14 13:49:05 -0400

    Adding Download selected button

--- src/js/plot_dataset.js
+++ src/js/plot_dataset.js
@@ -930,6 +930,49 @@ function exportCSV() {
     }
 }
 
+function downloadSelected(){
+    // Get a list of the selected rows
+    var idsToDownload = $("#small_dataset_table").bootstrapTable("getSelections")
+
+    // Loop through selections and download each using the IDs
+    for(let i = 0; i < idsToDownload.length; i++){
+        downloadJson(JSON.parse(JSON.stringify(idsToDownload[i])).id)
+    }
+
+}
+
+function downloadJson(jsonUID) {
+    const jsonUrl = `data/${jsonUID}.json`; // JSON file URL
+
+    // Fetch JSON data
+    fetch(jsonUrl)
+        .then(response => response.json())
+        .then(data => {
+
+            //alert(data)
+            // Create a blob from the JSON data
+            const blob = new Blob([JSON.stringify(data, null, 2)], {type: 'application/json'});
+
+            // Create an object URL for the blob
+            const url = URL.createObjectURL(blob);
+
+            // Create a link element
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = `${jsonUID}.json`; // specify downloaded file name
+
+            // Append the link element to the body
+            document.body.appendChild(link);
+
+            // Simulate a click on the link
+            link.click();
+
+            // Cleanup - remove the link element from the body
+            document.body.removeChild(link);
+        })
+        .catch(error => console.error('An error occurred:', error));
+}
+
 function switchRev() {
     toggleRev(switch_rev.checked)
     resetYAxis(plot_main)

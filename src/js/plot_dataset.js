9f4298b884a52ef0641c9ccc931e9938db4e720f
ahiser1117 <alex.hiser@gmail.com>
2023-08-17 17:36:40 -0400

    Update plot_dataset.js

--- src/js/plot_dataset.js
+++ src/js/plot_dataset.js
@@ -1006,8 +1006,10 @@ function toggleDataLoad(){
         $('#select_behavior').trigger('changed.bs.select');
     }
 
-    document.getElementById('plot-column').style.display = 'inline-block';
-
+    var plot = document.getElementById('plot-columns')
+    plot.style.display = 'none'
+    plot.offsetWidth;
+    plot.style.display = 'flex'
     // update the current URL
     let url = new URL(window.location.href);
     url.searchParams.set("datasets", dataload);
